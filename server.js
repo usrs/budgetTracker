4c2fdcfb4911a74b0e43f1ec65965758a7c9b386
Nathan <nathanmcneil@pm.me>
2020-06-17 23:37:03 -0700

    added database auth for app

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds137488.mlab.com:37488/heroku_g5lss56g",
+mongoose.connect(
+  "mongodb://nathan:G8K4zUuf4K2yJfi@ds137488.mlab.com:37488/heroku_g5lss56g",
   {
     useNewUrlParser: true,
     useFindAndModify: false,
